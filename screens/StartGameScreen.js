b41e21df2d62e0ea50e358dffd3dc43e6f8ef601
Kyle Zimmerman <kzdesignsios@gmail.com>
2023-03-08 09:00:46 -0800

    init styles for TextInput are done learned about different styling methods and reviewed some of the props TextInput component accept, main ones being keyboardType and maxLength

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,11 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.InputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.NumberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -24,5 +28,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  NumberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
