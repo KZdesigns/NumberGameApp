68ff1582b8f4491e9d5a1c77772fe413fc35aa92
Kyle Zimmerman <kzdesignsios@gmail.com>
2023-03-08 13:28:29 -0800

    finished up some button styling PrimaryButton component

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,9 +1,16 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 
 function PrimaryButton(props) {
   return (
-    <View>
-      <Text>{props.children}</Text>
+    <View style={styles.OutterButtonContainer}>
+      <View style={styles.InnerButtonContainer}>
+        <Pressable
+          style={({ pressed }) => pressed && styles.ButtonPressed}
+          onPress={props.onPress}
+        >
+          <Text style={styles.ButtonText}>{props.children}</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -11,7 +18,21 @@ function PrimaryButton(props) {
 export default PrimaryButton;
 
 const styles = StyleSheet.create({
-  Button: {
-    color: "blue",
+  OutterButtonContainer: {
+    borderRadius: 28,
+    margin: 5,
+    overflow: "hidden",
+  },
+  InnerButtonContainer: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: "white",
+  },
+  ButtonText: {
+    color: "#72063c",
+    textAlign: "center",
+  },
+  ButtonPressed: {
+    opacity: "0.5",
   },
 });
