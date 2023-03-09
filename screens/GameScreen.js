35ce956b1c36218a7975bba18d8cd8966c1fea01
Kyle Zimmerman <kzdesignsios@gmail.com>
2023-03-09 13:25:31 -0800

    finsihed basic layout for gamescreen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,27 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 function GameScreen() {
-  return <Text>Game Screen</Text>;
+  return (
+    <View style={styles.GameScreenContainer}>
+      <Text>Opponent's Guess</Text>
+      <Text>GUESS</Text>
+      <View>
+        <Text>High or Lower?</Text>
+        <Text>+-</Text>
+      </View>
+      <View>
+        <Text>LOG ROUNDS</Text>
+      </View>
+    </View>
+  );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  GameScreenContainer: {
+    flex: 1,
+    paddingTop: 50,
+    alignItems: "center",
+  },
+});
